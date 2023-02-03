bf1f5e5c2bd218d3b75e3f097d45ed3df4b9bff3
whydidIchoosephysics <93586814+whydidIchoosephysics@users.noreply.github.com>
2023-02-03 18:48:38 +0000

    creating url

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
- Your API key is ec5505d77c6eea9afb6162e232ff043c
+const apiKey = "ec5505d77c6eea9afb6162e232ff043c"
+
+ The base URL for your API calls should look like the following: 
+ 
+ 
+let latitudeCoord = ""
+let longitudeCoord = ""
+
+ let baseUrl = ("https://api.openweathermap.org/data/2.5/forecast?lat=" latitudeCoord + "&lon=" + longitudeCpprd + "&appid=" + apiKey)
