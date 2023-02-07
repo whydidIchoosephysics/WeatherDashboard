9967a9226be4fdacf6be826115e9004848eaf614
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-02-07 13:30:31 +0000

    Can visualise card weather data for 5 days when search is pressed

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -1,16 +1,15 @@
 const apiKey = "ec5505d77c6eea9afb6162e232ff043c";
 
 let citiesArea = $("#city-list");
+let daysArea = $("#weather-days");
+let cityBigName = $("#city-name");
 
-// http://api.openweathermap.org/data/2.5/forecast?lat=53.48&lon=-2.24&appid=ec5505d77c6eea9afb6162e232ff043c
-
-//   console.log(promise.list[0].dt_txt);
-//   console.log("Temperature: " + promise.list[0].main.temp + "°C");
-//   console.log("Humidity: " + promise.list[0].wind.speed + "KPH");
-//   console.log("Humidity: " + promise.list[0].main.humidity + "%");
+let searchedCities = [];
 
 function cityNameToCoordinates() {
   let cityName = $("#search-term").val().trim();
+
+  searchedCities.push(cityName);
 
   let urlCity =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -23,38 +22,27 @@ function cityNameToCoordinates() {
     method: "GET",
   }).then(function (promise) {
     console.log(promise);
-    console.log(promise[0].lat, promise[0].lon);
 
-    let lat = promise[0].lat;
-    let lon = promise[0].lon;
-    let coords = [lat, lon];
+    let lat = promise[0].lat.toFixed(3);
+    let lon = promise[0].lon.toFixed(3);
 
-    searchCityWeather(coords);
+    console.log(lat, lon);
+
+    searchCityWeather(lat, lon);
   });
 
   console.log(cityName);
 }
 
-$("#search-button").on("click", function (event) {
-  event.preventDefault();
-
-  let coords = cityNameToCoordinates();
-
-  searchCityWeather(coords);
-});
-
-function searchCityWeather(coordinates) {
-  let lat = coordinates[0];
-  let lon = coordinates[1];
-
+function searchCityWeather(latitude, longitude) {
   const baseURL = "http://api.openweathermap.org/data/2.5/forecast?";
 
   let finalURL =
     baseURL +
     "lat=" +
-    lat +
+    latitude +
     "&lon=" +
-    lon +
+    longitude +
     "&units=metric" +
     "&appid=" +
     apiKey;
@@ -67,11 +55,97 @@ function searchCityWeather(coordinates) {
     console.log(promise.city.name);
     console.log(promise.city.coord);
 
+    daysArea.empty();
+
     for (i = 0; i < promise.list.length; i = i + 8) {
-      console.log(promise.list[i].dt_txt);
-      console.log("Temperature: " + promise.list[i].main.temp + "°C");
-      console.log("Wind: " + promise.list[i].wind.speed + "KPH");
-      console.log("Humidity: " + promise.list[i].main.humidity + "%");
+      let date = promise.list[i].dt_txt.split(" ")[0];
+      let temperature = promise.list[i].main.temp;
+      let wind = promise.list[i].wind.speed;
+      let humidity = promise.list[i].main.humidity;
+
+      let tempText = "Temperature: " + temperature + "°C";
+      let windText = "Wind: " + wind + "KPH";
+      let humidityText = "Humidity: " + humidity + "%";
+
+      console.log(date);
+      console.log(tempText);
+      console.log(windText);
+      console.log(humidityText);
+
+      let card = $("<div>");
+      card.addClass("weather mini-day");
+      card.attr("id", "weather-mini" + [i / 8]);
+
+      let dateH4 = $("<h4>");
+      dateH4.addClass("date");
+      dateH4.attr("id", "weather-" + [i / 8]);
+      dateH4.text(date);
+
+      let temp = $("<p>").text(tempText);
+      temp.addClass("mini-text");
+
+      let win = $("<p>").text(windText);
+      win.addClass("mini-text");
+
+      let hum = $("<p>").text(humidityText);
+      hum.addClass("mini-text");
+
+      card.append(dateH4, temp, win, hum);
+      daysArea.append(card);
     }
   });
 }
+
+function renderButtons() {
+  searchedCities.forEach(function (name) {
+    citiesArea.empty();
+    let button = $("<button>");
+    button.addClass("btn btn-secondary btn-block");
+    button.attr("type", "button");
+    button.text(name);
+    citiesArea.append(button);
+  });
+}
+
+function renderCards() {
+  daysArea.empty();
+
+  // Create Big Weather
+
+  // Create small cards
+
+  let card = $("<div>");
+  card.addClass("weather mini-day");
+  card.attr("id", "weather-mini" + [i / 8]);
+
+  let dateH4 = $("<h4>");
+  dateH4.addClass("date");
+  dateH4.attr("id", "weather-" + [i / 8]);
+  dateH4.text(date);
+
+  let temp = $("<p>").text(tempText);
+  temp.addClass("mini-text");
+
+  let win = $("<p>").text(windText);
+  win.addClass("mini-text");
+
+  let hum = $("<p>").text(humidityText);
+  hum.addClass("mini-text");
+
+  card.append(dateH4, temp, win, hum);
+  daysArea.append(card);
+}
+
+$(document).ready(function () {
+  $("#search-button").on("click", function (event) {
+    event.preventDefault();
+
+    let coords = cityNameToCoordinates();
+
+    searchCityWeather(coords);
+
+    renderButtons();
+
+    console.log(searchedCities);
+  });
+});
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,4 +1,4 @@
-// const apiKey = "ec5505d77c6eea9afb6162e232ff043c";
+const apiKey = "ec5505d77c6eea9afb6162e232ff043c";
 
 //  The base URL for your API calls should look like the following:
 // let url =
@@ -90,65 +90,62 @@ function init() {
 
 init();
 
-// Code to tey from AI
+// Code to try from AI
 
 function getCoordinates(city) {
-  return $.ajax({
-    url: `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
-  });
+  $.ajax({
+    url: `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`,
+    method: "GET",
+    dataType: "json",
+  }).then({});
 }
 
 function getWeather(latitude, longitude) {
   return $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   let city = prompt("Enter the city name: ");
-  getCoordinates(city)
-    .done(function(data) {
-      let latitude = data.results[0].geometry.lat;
-      let longitude = data.results[0].geometry.lng;
-      getWeather(latitude, longitude)
-        .done(function(data) {
-          console.log(data);
-        });
+  getCoordinates(city).done(function (data) {
+    let latitude = data.results[0].geometry.lat;
+    let longitude = data.results[0].geometry.lng;
+    getWeather(latitude, longitude).done(function (data) {
+      console.log(data);
     });
+  });
 });
-
 
 // Code to try #2
 
 function getCoordinates(city) {
   return $.ajax({
     url: `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
 function getWeather(latitude, longitude) {
   return $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   let city = prompt("Enter the city name: ");
   getCoordinates(city)
-    .then(function(data) {
+    .then(function (data) {
       let latitude = data.results[0].geometry.lat;
       let longitude = data.results[0].geometry.lng;
       return getWeather(latitude, longitude);
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
     });
 });
@@ -157,37 +154,37 @@ $(document).ready(function() {
 
 function getCoordinates(city) {
   return $.ajax({
-    url: `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    url: `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`,
+    method: "GET",
+    dataType: "json",
   });
 }
 
 function getWeather(latitude, longitude) {
   return $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
-$(document).ready(function() {
-  $('#search-button').click(function() {
-    let city = $('#city-input').val();
+$(document).ready(function () {
+  $("#search-button").click(function () {
+    let city = $("#city-input").val();
     getCoordinates(city)
-      .then(function(data) {
+      .then(function (data) {
         let latitude = data.results[0].geometry.lat;
         let longitude = data.results[0].geometry.lng;
         return getWeather(latitude, longitude);
       })
-      .then(function(data) {
+      .then(function (data) {
         let weather = data.weather[0].main;
         let icon = data.weather[0].icon;
         let temperature = data.main.temp;
         let humidity = data.main.humidity;
         let windSpeed = data.wind.speed;
 
-        $('#weather-card').html(`
+        $("#weather-card").html(`
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Weather in ${city}</h5>
@@ -214,45 +211,45 @@ $(document).ready(function() {
 function getCoordinates(city) {
   return $.ajax({
     url: `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
 function getWeather(latitude, longitude) {
   return $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=<API_KEY>`,
-    method: 'GET',
-    dataType: 'json'
+    method: "GET",
+    dataType: "json",
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   let cities = [];
 
-  $('#search-button').click(function() {
-    let city = $('#city-input').val();
+  $("#search-button").click(function () {
+    let city = $("#city-input").val();
     getCoordinates(city)
-      .then(function(data) {
+      .then(function (data) {
         let latitude = data.results[0].geometry.lat;
         let longitude = data.results[0].geometry.lng;
         return getWeather(latitude, longitude);
       })
-      .then(function(data) {
+      .then(function (data) {
         cities.push({
           name: city,
-          weather: data
+          weather: data,
         });
 
-        let cityList = '';
+        let cityList = "";
         for (let i = 0; i < cities.length; i++) {
           cityList += `<button class="city-button" id="city-${i}">${cities[i].name}</button>`;
         }
 
-        $('#city-list').html(cityList);
+        $("#city-list").html(cityList);
 
-        $('.city-button').click(function() {
-          let id = $(this).attr('id').split('-')[1];
+        $(".city-button").click(function () {
+          let id = $(this).attr("id").split("-")[1];
           let weather = cities[id].weather;
 
           let temperature = weather.main.temp;
@@ -261,7 +258,7 @@ $(document).ready(function() {
           let weatherDescription = weather.weather[0].main;
           let icon = weather.weather[0].icon;
 
-          $('#weather-card').html(`
+          $("#weather-card").html(`
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title">Weather in ${city}</h5>
@@ -283,5 +280,3 @@ $(document).ready(function() {
       });
   });
 });
-
-
